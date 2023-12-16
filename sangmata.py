a554f4d7450c2d4f79404898d443c56bcbc528b7
Kadal <87251452+Djangonew@users.noreply.github.com>
2023-12-17 05:06:56 +0700

    Update sangmata.py

--- sangmata.py
+++ sangmata.py
@@ -190,7 +190,7 @@ dispatcher.add_error_handler(error)
 updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN,
-webhook_url = "https://+RAILWAY_APP_NAME+"..up.railway.app/" + TOKEN)
+webhook_url = "https://+RAILWAY_APP_NAME+"..up.railway.app/ + TOKEN)
 updater.start_webhook(listen="0.0.0.0",
 
                       port=PORT,
