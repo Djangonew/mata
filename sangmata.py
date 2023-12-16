2956487e8b8ba3c4a9fc10b39d40aaabb09852ce
Kadal <87251452+Djangonew@users.noreply.github.com>
2023-12-17 05:03:07 +0700

    typo

--- sangmata.py
+++ sangmata.py
@@ -189,7 +189,7 @@ dispatcher.add_error_handler(error)
 
 updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=TOKEN)
+                          url_path=TOKEN,
 webhook_url = 'https://'+RAILWAY_APP_NAME+'..up.railway.app/' + TOKEN)
 updater.start_webhook(listen="0.0.0.0",
 
